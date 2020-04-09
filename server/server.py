ffdd887468f9a51802ace041655575ea53a3f548
Bobby Moretti <bobmoretti@gmail.com>
2020-04-08 21:15:52 -0700

    Migrate from flask/waitress to quart/hypercorn
    This will allow http/2 and websockets for server event pushing.

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, send_from_directory, make_response, jsonify, abort
-from waitress import serve
+from quart import Quart, render_template, send_from_directory, make_response, jsonify, abort
+
 from unit_sidc import sidc_map
 
 from util import fixup_jsonlike
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+import asyncio
+
+import signal
 
 from coord import xz_to_lat_lon
+
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 def plain_text_response(x):
@@ -23,12 +32,13 @@ def collect_basic_unit_info(group):
     }
 
     print(f"group {g.name}")
+
     def to_latlon(p):
         lat, lon = xz_to_lat_lon(p['x'], p['y'])
         p['lat'] = lat
         p['lon'] = lon
         return p
-    
+
     points = [to_latlon(fixup_jsonlike(p.dict())) for p in g.points]
 
     data = {
@@ -43,20 +53,20 @@ def collect_basic_unit_info(group):
 
 
 def create_app(campaign):
-    app = Flask(__name__, static_folder='static',
+    app = Quart(__name__, static_folder='static',
                 template_folder='static')
 
     @app.route('/', defaults={'path': 'map.html'})
     @app.route('/static/<path:path>')
-    def send_static(path):
-        return send_from_directory('static', path)
+    async def send_static(path):
+        return await send_from_directory('server/static', path)
 
     def validate_coalition(c):
         if not c in campaign.mission.coalition:
             abort(404)
 
     @app.route('/game/plane_groups/<string:coalition>')
-    def render_plane_groups(coalition):
+    async def render_plane_groups(coalition):
         validate_coalition(coalition)
         groups = campaign.get_plane_groups(side=coalition)
 
@@ -68,7 +78,7 @@ def create_app(campaign):
         return jsonify([render_plane_group(g) for g in groups])
 
     @app.route('/game/ships/<string:coalition>')
-    def render_ship_groups(coalition):
+    async def render_ship_groups(coalition):
         validate_coalition(coalition)
         groups = campaign.get_ship_groups(side=coalition)
         return jsonify([collect_basic_unit_info(g) for g in groups])
@@ -78,4 +88,20 @@ def create_app(campaign):
 
 def run(campaign, port=80):
     app = create_app(campaign)
-    serve(app, port=port)
+
+    shutdown_event = asyncio.Event()
+
+    def _signal_handler(*_):
+        shutdown_event.set()
+
+    loop = asyncio.get_event_loop()
+    signal.signal(signal.SIGINT, _signal_handler)
+    signal.signal(signal.SIGTERM, _signal_handler)
+
+    config = Config()
+    config.bind = ["localhost:80"]  # As an example configuration setting
+
+    loop.run_until_complete(
+        serve(app, config, shutdown_trigger=shutdown_event.wait)
+    )
+
