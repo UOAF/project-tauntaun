59d42fe24658e98cae2e3d7bea3b92fd2ef591c7
Ádám Dóda <43262051+adamdoda@users.noreply.github.com>
2021-06-23 22:10:30 +0200

    Pydcs update (#83)
    * Mod: pydcs update
    
    * Mod: Set frontend version to v0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tauntaun-live-editor",
-    version="0.3.0",
+    version="0.3.1",
     author="UOAF",
     author_email="uoaf@fakemail.invalid",
     description="Project Tauntaun Live Editor",
