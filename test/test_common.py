3ba721de84fcb0723f75725deb49b2d50829fd6c
Bobby Moretti <bobmoretti@gmail.com>
2022-05-06 02:19:31 -0700

    Fix unit test as the setup code was relying on some pydcs internals that seem to have changed.

--- test/test_common.py
+++ test/test_common.py
@@ -1,8 +1,12 @@
 import os
 import sys
-sys.path.append(f"{os.path.dirname(os.path.realpath(__file__))}/../tauntaun_live_editor/dcs")
+
+sys.path.append(
+    f"{os.path.dirname(os.path.realpath(__file__))}/../tauntaun_live_editor/dcs"
+)
 
 import dcs
+from dcs.mapping import Vector2
 from dcs import Mission
 
 from tauntaun_live_editor.camp import Campaign
@@ -15,17 +19,21 @@ def create_mission() -> Mission:
     batumi.set_blue()
 
     usa = m.country("USA")
-    awacs = m.awacs_flight(
-        usa, "AWACS", dcs.planes.E_3A,
-        batumi, dcs.Point(batumi.position.x + 20000, batumi.position.y + 80000),
-        race_distance=120 * 1000, heading=90)
+    batumi_pos = batumi.position
+    batumi_offset_pos = batumi_pos + Vector2(20000, 80000)
+    awacs = m.awacs_flight(usa,
+                           "AWACS",
+                           dcs.planes.E_3A,
+                           batumi,
+                           batumi_offset_pos,
+                           race_distance=120 * 1000,
+                           heading=90)
 
-    escort = m.escort_flight(
-        usa, "Escort", dcs.planes.F_A_18C,
-        batumi, awacs)
+    escort = m.escort_flight(usa, "Escort", dcs.planes.F_A_18C, batumi, awacs)
     escort.load_pylon(dcs.planes.F_A_18C.Pylon2.Mk_82___500lb_GP_Bomb_LD, 2)
 
     return m
+
 
 def create_campaign() -> Campaign:
     campaign = Campaign()
